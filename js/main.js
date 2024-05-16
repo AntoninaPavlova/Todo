30f0b8c4d549d3966a881eee7aca17e931287532
tonyap92 <114396404+tonyap92@users.noreply.github.com>
2024-05-16 16:50:00 +0300

    I added a condition, if the value is empty in the modal window, then we block the save button. Added the function of deleting a task from the modal window.

--- js/main.js
+++ js/main.js
@@ -2,13 +2,10 @@
 
 // Необходимо реализовать список задач с использованием технологий:
 // HTML + (CSS или любой препроцессор) + JavaScript
-
 // В нашем приложении должно быть поле для ввода задачи, после нажатия на “Enter” оно должно добавляться в список.
 // Данную задачу можно удалить, можно отметить выполненной.
 // Под списком должны быть кнопки для удаления выполненных задач, а также для удаления всех задач.
-
 // Один из требований будет сохранение задач даже после обновления страницы, то есть при обновлении страницы добавленные задачи не должны пропадать.
-
 // Задание со звездочкой* Добавить возможность редактирования задачи.
 
 // CONSTS
@@ -20,34 +17,27 @@ const modal = document.querySelector(".todo-modal");
 const modalText = document.getElementById("todo-modal__text");
 const closeIcon = document.querySelector(".todo-modal__close");
 const btnSaveChanges = document.querySelector(".todo-modal__save");
+const btnDeleteTask = document.querySelector(".todo-modal__delete");
 
 // VARIABLES
 let tasks = [];
 
+const localStorageKey = "tasks";
+
 // FUNCTIONS
-
-// Date.now() не всегда стоит использовать в качестве уникального id.
-// + return можно убрать и оставить = () => Date.now()
-const generateUniqueId = () => {
-  return Date.now();
-};
-
+const generateUniqueId = () => Date.now();
 
 const handleEnterKey = (event) => {
   if (event.key === "Enter") {
     const task = taskInput.value.trim();
 
-
     if (task) {
       tasks.push({ id: generateUniqueId(), text: task, completed: false });
-      console.log("> handleEnterKey, tasks:", tasks);
-    }
-    // Получается что если задачи нет, то мы всё равно вызываем функцию renderResults + taskInput отчищаем
-    // А также в локалсторадж сохраняем, при этом ничего не добавив :D
 
-    renderResults();
-    taskInput.value = "";
-    saveInLocalStorage();
+      renderResults();
+      taskInput.value = "";
+      saveInLocalStorage();
+    }
   }
 };
 
@@ -55,8 +45,9 @@ const renderResults = () => {
   todoItem.innerHTML = "";
   tasks.forEach((task) => {
     todoItem.innerHTML += `
-    <input type="checkbox" class="todo-item__checkbox" onchange="completedTask(${task.id})" ${task.completed ? "checked" : ""
-      }>
+    <input type="checkbox" class="todo-item__checkbox" onchange="completedTask(${task.id})" ${
+      task.completed ? "checked" : ""
+    }>
         <span onclick="openModal(${task.id})" class="todo-item__text">${task.text}</span>
         <div onclick="deleteTask(${task.id})" class="todo-item__delete">
             <img src="img/delete.png" width="41" height="41" alt="delete">
@@ -67,8 +58,6 @@ const renderResults = () => {
 const deleteTask = (id) => {
   tasks = tasks.filter((task) => task.id !== id);
   renderResults();
-  // console.log надо убрать)
-  console.log("> deleteTask, tasks:", tasks);
   saveInLocalStorage();
 };
 
@@ -81,61 +70,67 @@ const completedTask = (id) => {
   });
 
   renderResults();
-  // тоже)
-  console.log("> completedTask, tasks:", tasks);
   saveInLocalStorage();
 };
 
 const deleteCompletedTask = () => {
   tasks = tasks.filter((task) => !task.completed);
   renderResults();
-  // и тут)
-  console.log("> deleteCompletedTask, tasks:", tasks);
   saveInLocalStorage();
 };
 
 const deleteAllTasks = () => {
   tasks = [];
   renderResults();
-  // и здесь)
-  console.log("> deleteAllTasks, tasks:", tasks);
   saveInLocalStorage();
 };
 
 const openModal = (id) => {
   const task = tasks.find((task) => task.id === id);
 
-  console.log("> openModal, id:", id);
-
   modalText.value = task.text;
-  console.log("> openModal, task.text:", task.text);
-
   modalText.setAttribute("name", id);
   modal.style.display = "block";
 };
 
 const editTask = () => {
-
-  // А нам действительно нужен parseInt?
-  // Мы ведь можем привести к числу другим, гораздо более лучшим образом :)
-  // В моём понимании parseInt это найти число из строки
-  const taskId = parseInt(modalText.getAttribute("name"));
-
-  console.log("> editTask, taskId:", taskId);
-  console.log("> editTask, modalText.value:", modalText.value);
+  const taskId = +modalText.getAttribute("name");
 
   tasks = tasks.map((task) => {
-    console.log(task.id === taskId);
     if (task.id === taskId) {
       return { ...task, text: modalText.value };
     }
     return task;
   });
 
-  console.log("> editTask, tasks:", tasks);
   renderResults();
   saveInLocalStorage();
   closeModal();
+};
+
+const checkModalText = () => {
+  if (modalText.value.trim() === "") {
+    btnSaveChanges.disabled = true;
+    btnSaveChanges.style.opacity = "0.4";
+  } else {
+    btnSaveChanges.disabled = false;
+    btnSaveChanges.style.opacity = "1";
+  }
+};
+
+const deleteThisTask = () => {
+  const taskId = +modalText.getAttribute("name");
+
+  const confirmDelete = confirm("Вы уверены, что хотите удалить эту задачу?");
+
+  if (confirmDelete) {
+    tasks = tasks.filter((task) => task.id !== taskId);
+    renderResults();
+    saveInLocalStorage();
+    closeModal();
+  } else {
+    alert("Удаление отменено.");
+  }
 };
 
 const closeModal = () => {
@@ -143,28 +138,26 @@ const closeModal = () => {
 };
 
 const saveInLocalStorage = () => {
-  // локал сторадж ключи стоит выносить в отдельные переменные
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+  localStorage.setItem(localStorageKey, JSON.stringify(tasks));
 };
 
 const getFromLocalStorage = () => {
-  // локал сторадж ключи стоит выносить в отдельные переменные
-  const savedTasks = localStorage.getItem("tasks");
+  const savedTasks = localStorage.getItem(localStorageKey);
 
   if (savedTasks) {
     tasks = JSON.parse(savedTasks);
   }
 
   renderResults();
-  console.log("> getFromLocalStorage, tasks:", tasks);
 };
 
 getFromLocalStorage();
 
 // LISTENERS
-// Думаю слушатели стоит вынести на самый верх
 taskInput.addEventListener("keyup", handleEnterKey);
 btnRemoveCompleted.addEventListener("click", deleteCompletedTask);
 btnRemoveAll.addEventListener("click", deleteAllTasks);
 closeIcon.addEventListener("click", closeModal);
 btnSaveChanges.addEventListener("click", editTask);
+modalText.addEventListener("input", checkModalText);
+btnDeleteTask.addEventListener("click", deleteThisTask);
